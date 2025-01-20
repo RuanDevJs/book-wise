a3ec3d4641c2b3c7a04321855418b9337287db0e
RuanDevJs <ruanelpidio@hotmail.com>
2025-01-20 17:34:13 -0300

    Added more images

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,7 +12,10 @@ const IMAGES = [
   "https://m.media-amazon.com/images/I/71JpZHEGvWL._UF894,1000_QL80_.jpg",
   "https://m.media-amazon.com/images/I/91mH07aKF8L._AC_UF1000,1000_QL80_.jpg",
   "https://m.media-amazon.com/images/I/A1jk2ppjsCL._AC_UF1000,1000_QL80_.jpg",
-  "https://m.media-amazon.com/images/I/A1Sq8q77OfL._AC_UF1000,1000_QL80_.jpg"
+  "https://m.media-amazon.com/images/I/A1Sq8q77OfL._AC_UF1000,1000_QL80_.jpg",
+  "https://m.media-amazon.com/images/I/81MLf-pgXrL._AC_UF1000,1000_QL80_.jpg",
+  "https://m.media-amazon.com/images/I/71SKqtvjBAL.jpg",
+  "https://m.media-amazon.com/images/I/71TY7kJBo9L._AC_UF1000,1000_QL80_.jpg"
 ]
 
 
